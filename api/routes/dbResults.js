c2718a135dfafbb33b30e6b1259e056134c2a085
Ethanscottmyers <59705581+Ethanscottmyers@users.noreply.github.com>
2020-07-31 16:16:34 -0400

    working on front-end functionality

--- api/routes/dbResults.js
+++ api/routes/dbResults.js
@@ -13,29 +13,38 @@ const connection = mysql.createConnection({
 
 router.get("/", (req, res, next) => {
   let id = req.query.id;
-  let sem = req.query.sem;
-  let leeway = req.query.leeway;
-  let percent = req.query.percent;
+  let curr = parseInt(req.query.curr);
+  let resSem = parseInt(req.query.resSem);
+  let leeway = parseInt(req.query.leeway);
+  let percent = parseFloat(req.query.percent);
   connection.query(
-    `SELECT * FROM Grades WHERE studentID = '${id}' AND termsFromStudentStart <= ${sem};`,
+    `SELECT * FROM Grades WHERE studentID = '${id}' AND termsFromStudentStart <= ${curr};`,
     (err, results, fields) => {
       if (err) throw err;
       if (isNull(results)) throw "Error! Invalid student ID. ";
       // historyQuery = `SELECT * FROM Grades WHERE studentID IN \n(SELECT studentID FROM Students WHERE studentID != '${id}' AND EXISTS \n`;
-      historyQuery = `SELECT studentID FROM Students WHERE studentID != '${id}' AND EXISTS \n`;
+      historyQuery =
+        `SELECT * FROM Grades WHERE termsFromStudentStart = ${
+          curr + resSem
+        } AND studentID IN \n` +
+        `(SELECT studentID FROM Students WHERE studentID != '${id}' AND EXISTS \n`;
       for (let i = 0; i < results.length; i++) {
+        //FIX DIVIDE BY ZERO
         let grade = results[i];
         historyQuery +=
           `(SELECT 1 FROM Grades WHERE Grades.studentID = Students.studentID AND catalogNumber = ${grade.catalogNumber} ` +
           `AND ABS(termsFromStudentStart - ${grade.termsFromStudentStart}) <= ${leeway} ` +
-          `AND (LEAST(parseGrade('${grade.grade}'), parseGrade(grade)) / GREATEST(parseGrade('${grade.grade}'), parseGrade(grade))) >= ${percent}) `;
+          `AND IF(LEAST(parseGrade('${grade.grade}'), parseGrade(grade)) = 0, TRUE, ` +
+          `(LEAST(parseGrade('${grade.grade}'), parseGrade(grade)) / GREATEST(parseGrade('${grade.grade}'), parseGrade(grade))) ` +
+          `>= ${percent})) `;
         if (i < results.length - 1) {
           historyQuery += `AND EXISTS\n`;
         } else {
           // historyQuery += `\n) ORDER BY studentID ASC, catalogNumber ASC, term ASC;`;
-          historyQuery += `ORDER BY studentID;`;
+          historyQuery += `ORDER BY studentID);`;
         }
       }
+      // console.log(historyQuery);
       connection.query(historyQuery, (err, results, fields) => {
         if (err) throw err;
         res.send(results);
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [apiResponse, setAPIResponse] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-  useEffect(() => {
-    fetch("http://localhost:9000/testAPI?id=1&sem=3&leeway=1&percent=1 ")
-      .then((res) => res.text())
-      .then((res) => setAPIResponse(res));
-  });
+  // useEffect(() => {
+  //   fetch(
+  //     "http://localhost:9000/database-results?id=39E1DBE6E27576F37410FC3E1FE2B53AE250E1CA&curr=3&resSem=1&leeway=2&percent=1"
+  //   )
+  //     .then((res) => res.text())
+  //     .then((res) => setAPIResponse(res));
+  // });
 
+  async function submitButton() {
+    setSubmitted(true);
+    console.log("in submitted");
+    let res = await fetch(
+      "http://localhost:9000/database-results?id=39E1DBE6E27576F37410FC3E1FE2B53AE250E1CA&curr=3&resSem=1&leeway=2&percent=1"
+    );
+    res = await res.text();
+    setAPIResponse(res);
+  }
+
+  function clearButton() {
+    setAPIResponse("");
+    setSubmitted(false);
+  }
   return (
-    <div className="App">
-      <p className="App-intro">{apiResponse}</p>
+    <div>
+      <button onClick={submitButton}>Query</button>
+      <button onClick={clearButton}>Clear</button>
+      <p>{apiResponse}</p>
     </div>
   );
 }
